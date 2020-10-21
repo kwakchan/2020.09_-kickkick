6635bc2600509b7e5a34780ae92755b4e4bc3e02
kwakchan <kwakchan>
2020-10-22 01:55:58 +0900

    node js를 이용해서 서버 연결과 mysql연동

---
+++ nodejs/mysql.js
@@ -0,0 +1,18 @@
+var mysql      = require('mysql');
+// 비밀번호는 별도의 파일로 분리해서 버전관리에 포함시키지 않아야 합니다. 
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  database : 'kickkick'
+});
+  
+connection.connect();
+  
+connection.query('SELECT * FROM matching', function (error, results, fields) {
+    if (error) {
+        console.log(error);
+    }
+    console.log(results);
+});
+  
+connection.end();
